ccf2a79e32ed96184769a221d92a2aeb40bddd23
Jaroslaw Zabiello <hipertracker@gmail.com>
2015-08-12 04:02:42 +0100

    Update actions.js

--- src/actions.js
+++ src/actions.js
@@ -4,18 +4,6 @@
 
 export const ADD_TODO = 'ADD_TODO';
 export const REMOVE_TODO = 'REMOVE_TODO';
-//export const COMPLETE_TODO = 'COMPLETE_TODO';
-//export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER'
-
-/*
- * other constants
- */
-
-//export const VisibilityFilters = {
-//    SHOW_ALL: 'SHOW_ALL'
-//    SHOW_COMPLETED: 'SHOW_COMPLETED',
-//    SHOW_ACTIVE: 'SHOW_ACTIVE'
-//};
 
 /*
  * action creators
@@ -28,11 +16,3 @@ export function addTodo(text) {
 export function removeTodo(index) {
     return {type: REMOVE_TODO, index};
 }
-//
-//export function completeTodo(index) {
-//    return {type: COMPLETE_TODO, index};
-//}
-//
-//export function setVisibilityFilter(filter) {
-//    return {type: SET_VISIBILITY_FILTER, filter};
-//}
